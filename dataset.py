91687590d0e0053a36b600cdfff9c237d0202233
ArmanZarei <armanzarei1378@gmail.com>
2022-10-26 19:54:15 +0330

    EEG Dataset

---
+++ dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+
+
+class EEGDataset(Dataset):
+  def __init__(self, windows, labels, transform=None):
+    self.windows = windows
+    self.labels = labels
+    self.transform = transform
+  
+  def __len__(self):
+    return self.labels.shape[0]
+  
+  def __getitem__(self, idx):
+    x = self.windows[idx]
+    if self.transform:
+      x = self.transform(self.windows[idx])
+
+    return x, self.labels[idx]
